c493929693cacc30bff2120bc9f57cde2a12755c
AbdulRahman Sharief <111624962+AbdulRahman-Sharief@users.noreply.github.com>
2023-11-19 03:18:54 +0200

    Add Cover Skeleton and the main section Skeleton in the document page

--- notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -4,6 +4,7 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface DocumentIdProps {
   params: {
@@ -14,7 +15,20 @@ const DocumentIdPage = ({ params }: DocumentIdProps) => {
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   });
-  if (document === undefined) return <div>Loading ...</div>;
+  if (document === undefined)
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl  mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-14 w-[80%]" />
+            <Skeleton className="h-14 w-[40%]" />
+            <Skeleton className="h-14 w-[60%]" />
+          </div>
+        </div>
+      </div>
+    );
   if (document === null) return <div>Not found</div>;
 
   return (
