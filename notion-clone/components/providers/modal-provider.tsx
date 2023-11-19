ff4b81446820a33a0bef957ff5162cbaf0485eb5
AbdulRahman Sharief <111624962+AbdulRahman-Sharief@users.noreply.github.com>
2023-11-19 02:55:51 +0200

    Cover image File upload

--- notion-clone/components/providers/modal-provider.tsx
+++ notion-clone/components/providers/modal-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { SettingsModal } from "../modals/settings-modal";
-
+import { CoverImageModal } from "../modals/cover-image-modal";
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
   useEffect(() => {
@@ -11,6 +11,7 @@ export const ModalProvider = () => {
   return (
     <>
       <SettingsModal />
+      <CoverImageModal />
     </>
   );
 };
